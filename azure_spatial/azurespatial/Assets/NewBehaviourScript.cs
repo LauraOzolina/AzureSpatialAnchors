f403cbb8a11ada1e6bf73a16955cb130b11c5b7d
LauraOzolina <laura.ozolina15@va.lv>
2021-06-16 20:38:52 +0300

    Spatial innit

---
+++ azure_spatial/azurespatial/Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.Azure.SpatialAnchors;
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloudSpatialAnchorSession cloudsession;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
